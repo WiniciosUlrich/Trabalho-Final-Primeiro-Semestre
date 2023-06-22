778af0b6d1ac6be6a343f07b7bb9a57f4ae95b29
Winicios Ulrich <winiciosivanulrich10@gmail.com>
2023-06-22 20:47:53 -0300

    inicio

---
+++ src/TrabalhoFinal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TrabalhoFinal 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        
+    }    
+}
